c97876c50de66615448e23a6b98620e15d514f2d
M1ghtyPirate <103485029+M1ghtyPirate@users.noreply.github.com>
2023-05-03 13:27:14 +0300

    Логин, пользователь БД, строка подключения к localhost, отключение кнопок редактирования лесничества, иконка сайта.

---
+++ ForestryDB/SQL/DataBase/AttachToRemote.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (SELECT * FROM master.sys.databases WHERE name='forest')
+CREATE DATABASE forest   
+    ON (FILENAME = N'/var/opt/mssql/data/forest.mdf'),   
+    (FILENAME = N'/var/opt/mssql/data/forest_Log.ldf')   
+    FOR ATTACH;
+
+--EXEC sp_detach_db 'forest', 'true';
---
+++ ForestryDB/SQL/Users/user_ForestryWeb.sql
@@ -0,0 +1,14 @@
+﻿USE [forest]
+
+CREATE LOGIN [ForestryWeb] WITH PASSWORD = 'P@ssw0rd';
+CREATE USER [ForestryWeb] FOR LOGIN [ForestryWeb]; 
+
+GRANT SELECT TO [ForestryWeb];
+
+GRANT INSERT TO [ForestryWeb];
+
+GRANT UPDATE TO [ForestryWeb];
+
+GRANT DELETE TO [ForestryWeb];
+
+GRANT EXECUTE TO [ForestryWeb];
